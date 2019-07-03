8bdd82610c97ccd9ce80cb7cd7a6593b0b7f5807
Bruce MacKinnon <bruce@mackinnon.com>
2019-07-02 21:30:39 -0400

    Sin test working, testing microphone on bypass.

--- KC1FSZ-WM8510-Test2.ino
+++ KC1FSZ-WM8510-Test2.ino
@@ -9,6 +9,9 @@
 #define I2S_MCLK_DIVIDE 423
 
 // WM8510 Control masks
+#define WM8510_02_ADCEN       (0b000000001)
+#define WM8510_02_INPPGAEN    (0b000000100)
+
 #define WM8510_03_SPKNEN      (0b001000000)
 #define WM8510_03_SPKPEN      (0b000100000)
 #define WM8510_03_SPKMIXEN    (0b000000100)
@@ -19,9 +22,80 @@
 
 #define WM8510_06_CLKSEL      (0b100000000)   // PLL
 
+#define WM8510_2A_DACOSR128   (0b000001000)   // DAC oversampling 128x
+
 #define WM8510_2C_MVBSEL      (0b100000000)
 #define WM8510_2C_MICPNINPPGA (0b000000010)
 #define WM8510_2C_MICP2INPPGA (0b000000001)
+
+#define WM8510_32_BYP2SPK     (0b000000010) 
+#define WM8510_32_DAC2SPK     (0b000000001) 
+
+// Sample frequency
+const int Fs = 8000;
+// Tone frequency
+const int Ftone = 1500;
+// Tone amplitude (16-bit, signed)
+const int Atone = 32766;
+
+// Used to keep track of L and R sides of frame
+unsigned int FrameCounter = 0;
+// How far we jump through the phase on each sample (depends on Ftone)
+unsigned int PhaseStep = 0;
+// Current phase pointer
+unsigned int PhasePtr = 0;
+
+// ===== sin() look-up table ===========================================
+// This is the number of phase buckets we manage.  More buckets means 
+// a smoother transition through the baseband signals.
+const unsigned int PhaseRange = 1024;
+// The LUT only has to cover 90 degrees of the phase range because 
+// we have quadrant translation.
+const unsigned int LutSize = PhaseRange / 4;
+// This is the LUT (filled during setup())
+int SinLut[LutSize];
+
+// Build the look-up table.  This will only cover the first 90 
+// degrees of the sin() function.
+void setupLut(int* lut,unsigned int phaseRange,int scale) {
+  for (unsigned int i = 0; i < phaseRange / 4; i++) {
+    // rad ranges from 0 to PI/2
+    float rad = ((float)i / (float)phaseRange) * 2.0 * 3.1415926;
+    // s ranges from 0 to 1
+    float s = sin(rad);
+    // lut ranges from 0 to scale
+    // TODO: INVESTIGATE ROUNDING
+    lut[i] = ((float)scale * s);
+  }
+}
+
+// This function takes an integer phase and returns the sin() value.  Quadrant
+// translation is used to save memory.
+//
+// ph - Integer phase from 0 to range-1
+// lut - Pointer to a look-up-table that has range / 4 entries from 0 to pi/2 radians 
+//   or 0 to 90 degrees.
+// lutSize - Total size of look up table. 1/4 or phaseRange.
+//
+int sinWithQuadrant(unsigned int ph,int* lut,unsigned int lutSize,int scale) {
+  // Figure out which quandrant we're in and adjust accordingly.  0, 1, 2, or 3.
+  const unsigned int quadrant = ph / lutSize;
+  const unsigned int range = lutSize * 4;
+  // There are some special cases here
+  if (ph == lutSize) {
+    return scale;
+  } else if (ph == lutSize * 3) {
+    return -scale;
+  } else if (quadrant == 0) {
+    return lut[ph];
+  } else if (quadrant == 1) {
+    return lut[range / 2 - ph];
+  } else if (quadrant == 2) {
+    return -lut[ph - range / 2];
+  } else {
+    return -lut[range - ph];
+  }
+}
 
 void strobePin(int pin) {
   digitalWrite(pin,1);
@@ -51,9 +125,6 @@ void writeWM8510Register(unsigned int addr,unsigned int data) {
   digitalWrite(PIN_WM8510_CSB,1);
 }
 
-float cycle[256];
-int cycleSize = 256;
-
 void setup() {
 
   delay(50);
@@ -61,11 +132,10 @@ void setup() {
   Serial.println("Hello?");
   delay(50);
 
-  float fullCycle = 2 * 3.1415926;
-  for (int i = 0; i < cycleSize; i++) {
-    cycle[i] = cos(fullCycle * ((float)i/(float)cycleSize));
-  }
-
+  // Load up the sin() look-up table
+  setupLut(SinLut,PhaseRange,Atone);
+  // How far to step through the cycle at each stample
+  PhaseStep =  (PhaseRange * Ftone) / Fs;
   
   pinMode(13,OUTPUT);
 
@@ -88,7 +158,6 @@ void setup() {
   // Normally high
   digitalWrite(PIN_WM8510_CSB,1);
 
-
   // WN8510 Power on sequence
   // Wait for supply voltage to settle
   delay(50);
@@ -106,12 +175,23 @@ void setup() {
   writeWM8510Register(0x06,0);
   // Sample rate to 8kHz
   writeWM8510Register(0x07,0b000001010);
-  // Set microphone bias level and hook up MICN and MICP
-  //writeWM8510Register(0x2c,
-  //  WM8510_2C_MVBSEL | WM8510_2C_MICPNINPPGA | WM8510_2C_MICP2INPPGA);
-  // Set DACMU=0
-  writeWM8510Register(0x0a,0);
-  writeWM8510Register(0x0b,0b011110000);
+
+  // Enable the microphone PGA
+  writeWM8510Register(0x02,WM8510_02_INPPGAEN);  
+  // Set microphone bias level and hook up MICN and MICP to the PGA
+  writeWM8510Register(0x2c,
+    WM8510_2C_MICPNINPPGA | WM8510_2C_MICP2INPPGA);
+  // Set the PGA gain to full-scale, PGA not muted
+  writeWM8510Register(0x2d,0b000111111);
+   
+  // Set DACMU=0 | Set DAC oversampling 128x (best quality)
+  writeWM8510Register(0x0a,WM8510_2A_DACOSR128);
+  
+  // TEMP: Adjust the speaker mixer stage so that it takes the bypass path 
+  writeWM8510Register(0x32,WM8510_32_BYP2SPK);
+  
+  // Speaker Gain
+  writeWM8510Register(0x0b,0b011111000);
 
   // ----- I2S Configuration
   // Configure the Teensy 3.2 pins per the reference and wiring
@@ -130,7 +210,7 @@ void setup() {
   I2S0_TMR = 0;
   // Set the high water mark for the FIFO.  This determines when the interupt
   // needs to fire.
-  I2S0_TCR1 = I2S_TCR1_TFW(1);
+  I2S0_TCR1 = I2S_TCR1_TFW(2);
   // Asynchronous mode | Bit Clock Active Low | Master Clock 1 Selected |
   // Bit Clock generated internally (master) | Bit Clock Divide
   // Setting DIV=3 means (3+1)*2=8 division of MCLK
@@ -164,59 +244,36 @@ void loop() {
   */
 }
 
-uint32_t frameCounter = 0;
-uint32_t diag0 = 0;
-
-float fs = 8000.0;
-// Tone frequency
-float ft = 1000.0;
-// How far to step through the cycle at each stample
-float step = ((float)cycleSize / (fs / ft));
-// Phase accumulator
-float ptr = 0;
-
-/*
- * Utility function that looks at the read/write ponters on the 
- * FIFO to determine if it is completely full yet.  The FIFO
- * pointers are 3-bit values.
- */
-bool isFIFOFull() {
-    // Read the FIFO pointers
-    uint32_t v = I2S0_TFR0;
-    uint32_t wfp = (v & 0b11110000000000000000) >> 16;
-    uint32_t rfp = (v & 0b1111);
-    // Full happens when the pointers are the same except for the MSB.
-    if ((wfp & 0b111) == (rfp & 0b111) && wfp != rfp) {
-      return true;
-    } else {
-      return false;
-    }
-}
+int Sig = 0;
 
 /**
- * This function writes as much as possible into the FIFO.
+ * This function writes a few words into the FIFO.
  */
 void tryWrite() {
-  // Loop
-  while (!isFIFOFull()) {
-    
-    frameCounter++;
-    
+  // A fixed amount of data is written into the FIFO each cycle (4 words)
+  for (int i = 0; i < 4; i++) {
+    FrameCounter++;
     // Data counter only advances at the start of a frame.
-    if (frameCounter % 2 == 1) {
+    if (FrameCounter % 2 == 1) {
+      // SINE WAVE
       // Advance through the sample space, wrapping as needed
-      ptr += step;
-      if (ptr >= (float)cycleSize) {
-        ptr -= (float)cycleSize;
+      PhasePtr += PhaseStep;
+      // Check for wrap
+      if (PhasePtr >= PhaseRange) {
+        PhasePtr -= PhaseRange;
       }
-      // This ranges from -1 to +1
-      float sample = cycle[(int)ptr];
-      // This ranges from -32,766.0 to 32,766.0
-      float scaled = ((float)(0xfffe) / 2.0) * sample;
-      // Change to signed integer (twos complement)
-      int scaledInt = scaled;
+      // This ranges from -Atone to +Atone
+      int scaledInt = sinWithQuadrant(PhasePtr,SinLut,LutSize,Atone);
       // Write a value.  This automatically advances the write pointer
-      I2S0_TDR0 = scaledInt;    
+      I2S0_TDR0 = scaledInt;  
+      /* SQUARE WAVE
+      Sig++;
+      if (Sig % 32 >= 16) {
+        I2S0_TDR0 = 0x5555;       
+      } else {
+        I2S0_TDR0 = 0xaaaa;               
+      }
+      */
     } else {
       I2S0_TDR0 = 0;    
     }
